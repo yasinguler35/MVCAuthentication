40f5637a004ac7a6401174ab6f4df92d72334178
yasinguler35 <yasinguler007@gmail.com>
2021-05-12 17:56:38 +0300

    AUTContext eklendi ve entity freamwork ve bootstrap 4.0.0 sürümü kullanıldı

---
+++ MVCAuthentication/DAL/AUTContext.cs
@@ -0,0 +1,24 @@
+﻿using MVCAuthentication.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace MVCAuthentication.DAL
+{
+    public class AUTContext:DbContext
+    {
+        public AUTContext() : base("AUTVeritabani")
+        {
+
+        }
+        public DbSet<User> User { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //tabloların sonuna gelen s takısını silmek için
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
