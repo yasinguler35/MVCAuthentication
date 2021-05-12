9c62de5d81b5a6a7b5e31f4c8865fca60f344457
yasinguler35 <yasinguler007@gmail.com>
2021-05-12 18:04:26 +0300

    vlgoin post methodu tanımlandı view yazıldı ve veritabanından kontrol edildi kullanıcı adı ve şifre var mı diye

--- MVCAuthentication/Controllers/SecurityController.cs
+++ MVCAuthentication/Controllers/SecurityController.cs
@@ -1,17 +1,38 @@
-﻿using System;
+﻿using MVCAuthentication.DAL;
+using MVCAuthentication.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace MVCAuthentication.Controllers
 {
     public class SecurityController : Controller
     {
+        AUTContext db = new AUTContext();
         // GET: Security
         public ActionResult Login()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            //veritabanından şifre kullanıcı adı kontrol ediliyor
+            var userInDb = db.User.FirstOrDefault(u => u.userName == user.userName && u.password == user.password);
+            if (userInDb != null)
+            {
+                FormsAuthentication.SetAuthCookie(user.userName, false);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Mesaj = "Geçersiz Kullanıcı,şifre veya kullanıcı adı hatalı";
+                return View();
+            }
+
         }
     }
 }
