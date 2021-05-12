80b81736ae0aa39c68115752c41a6479bc452b6e
yasinguler35 <yasinguler007@gmail.com>
2021-05-12 17:50:16 +0300

    model klasorune user tablosu eklendi

---
+++ MVCAuthentication/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVCAuthentication.Models
+{
+    [Table("User")]
+    public class User
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int userId { get; set; }
+        [DisplayName("Kullanıcı Adı"), StringLength(50), Required]
+        public string userName { get; set; }
+        [DisplayName("Şifresi"), StringLength(50), Required]
+        public string password { get; set; }
+        public int rank { get; set; }
+    }
+}
